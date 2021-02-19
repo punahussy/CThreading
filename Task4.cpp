0c8ed7bb7d84d40a8bbb0963d5922461f0cf228a
Artem <raketenwerfer@yandex.ru>
2021-02-19 23:49:14 +0500

    готово к добавлению мьютексов

--- Task4.cpp
+++ Task4.cpp
@@ -11,32 +11,33 @@
 using namespace std;
 
 #define DATABASE_SIZE 64
-#define WRITE_OPERATIONS 15
+#define WRITE_OPERATIONS 5
 #define READ_OPERATIONS 15
-#define WRITERS_READERS_CNT 5
+#define WRITERS_READERS_CNT 3
 
 string thread_symbols = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
 
-//Инкапсулированная в виде базы данных очередь строк
+//Инкапсулированная в виде базы данных очередь символов
 class Database {
     private:
-        queue<string> data;  
+        queue<char> data;  
         int dataWritten = 0;   
     public:
-        void Write(string toWrite) {
+        //Запись данных в базу
+        void Write(char toWrite) {
             if (data.size() < DATABASE_SIZE) {
                 data.push(toWrite);
-                dataWritten += toWrite.length();
+                dataWritten++;
             }
         }
-        string Read() {
+        char Read() {
             if (data.size() > 0) {
-                string read_data = data.front();
+                char read_data = data.front();
                 data.pop();
-                dataWritten -= read_data.length();
+                dataWritten--;
                 return read_data;
             }
-            else return "";
+            else return '\n';
         }
         int GetDataWritten() {
             return dataWritten;
@@ -45,23 +46,17 @@ class Database {
 
 Database db;
 
-int random_in_range(int begin, int end) {
-    random_device rd;
-    mt19937 generator(rd());
-    uniform_int_distribution<int> uni(begin, end);
-
-    return uni(generator);
-}
-
 //Метод записи для потока
 void *Writer(void *args) {
     int *writer_Id = (int *) args;
     for (int i = 0; i < WRITE_OPERATIONS; i++) {
-        string data = "";
-        data.insert(0, i, thread_symbols[*writer_Id]);
+        char data = thread_symbols.at(*writer_Id);
         db.Write(data);
-        cout <<
-            to_string(i) + ". Записано " + to_string(data.length()) + " символов \n";
+        string msg = "Записан символ ";
+        msg.append(1, data);
+        msg.append(" в потоке ");
+        msg.append(to_string(*writer_Id));
+        cout << msg +"\n";
         sleep(1);
     }
     return 0;
@@ -70,10 +65,9 @@ void *Writer(void *args) {
 //Метод чтения для потока
 void *Reader(void* args) {
     for (int i = 0; i < READ_OPERATIONS; i++) {
-        int pos = random_in_range(0, DATABASE_SIZE - 1);
-        string data = db.Read();
+        char data = db.Read();
         cout << 
-            to_string(i) + ") Считано " + to_string(data.length()) + " символов \n";
+            to_string(i) + ") Считано символ" + to_string(data) + "  \n";
         sleep(1);
     }
     return 0;
@@ -106,11 +100,11 @@ void *init_readers(void *args) {
 //Параллельно создает читателей и писателей
 void InitThreads() {
     pthread_t writers_creator;
-    pthread_t readers_creator;
+    //pthread_t readers_creator;
     pthread_create(&writers_creator, NULL, init_writers, NULL);
-    pthread_create(&readers_creator, NULL, init_readers, NULL);
+    //pthread_create(&readers_creator, NULL, init_readers, NULL);
     pthread_join(writers_creator, NULL);
-    pthread_join(readers_creator, NULL);
+    //pthread_join(readers_creator, NULL);
 }
 
 
